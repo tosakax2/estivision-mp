189c895329d1f20e9061d39a6cbc5453376b0bce
tosakax2 <tosakax2@gmail.com>
2025-08-12 15:04:49 +0900

    コメントを修正

--- src/estv/trackers/osc_sender.py
+++ src/estv/trackers/osc_sender.py
@@ -1,6 +1,13 @@
 from pythonosc.udp_client import SimpleUDPClient
 
 
+# Unityの座標系と同じ、左利き座標系、+y
+# 位置(x, y, z)は 1.0f = 1m
+# 回転(z, y, x)はクォータニオン、単位は度(degree)、回転値はワールド座標系
+# VRChatでサポートされている部位は、腰・胸・肘×2・膝×2・足×2の8か所
+# 1から8までのどの番号がどの部位になるのかは決まっておらず、VRChat内でキャリブレーションした際にOSCにより入力された座標のIKポインターとVRChat上のアバターのトラッカーに追従する部位が自動的に対応付けられる仕様なので、どの番号にどの部位を割り当てるかは送信元のアプリケーションの任意
+
+
 class VRChatOscTrackerSender:
 
     def __init__(self, ip="127.0.0.1", port=9000):
@@ -8,8 +15,6 @@ class VRChatOscTrackerSender:
 
 
     def send_tracker(self, tracker_index, position, rotation):
-        # 位置(x, y, z)はメートル単位、回転(x, y, z)はクォータニオンで送信
-        # tracker_indexは1～8（1=腰, 2=左足, 3=右足, ...など）
         self.client.send_message(
             f"/tracking/trackers/{tracker_index}/position",
             [float(position[0]), float(position[1]), float(position[2])]
